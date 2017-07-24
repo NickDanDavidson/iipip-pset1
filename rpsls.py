31751a5df6bd61804fb0802acca58a4340cf3b13
nickdandave <nickdandavidson@gmail.com>
2017-07-24 16:45:25 -0400

    Remove unnecessary function definitions and variables

--- rpsls.py
+++ rpsls.py
@@ -1,29 +1,20 @@
 import random
 
 CHOICES = ["rock", "Spock", "paper", "lizard", "scissors"]
-CHOICE_BY_NUM = {num: choice for num,choice in zip(range(0, 5), CHOICES)}
-CHOICE_BY_NAME = {choice: num for num,choice in zip(range(0, 5), CHOICES)}
-
-def num_to_name(num):
-    """ Convert numbers 0-4 to their corresponding choice names. """
-
-    return CHOICE_BY_NUM[num]
-
-def name_to_num(name):
-    """ Convert choice string to a number between 0 and 4. """
-
-    return CHOICE_BY_NAME[name]
 
 def play(choice):
     """ Simulate a game of Rock/Paper/Scissors/Lizard/Spock. """
 
-    player_num = name_to_num(choice)
+    # Compare choices numerically
+    player_num = CHOICES.index(choice)
     comp_num = random.randrange(0, 5)
     difference = (player_num - comp_num) % 5
-    comp_name = num_to_name(comp_num)
+
+    # Convert computer choice to string
+    comp_choice = CHOICES[comp_num]
 
     print("Player chooses", choice)
-    print("Computer chooses", comp_name)
+    print("Computer chooses", comp_choice)
 
     if difference in [3, 4]:
         outcome = "Computer wins!"
